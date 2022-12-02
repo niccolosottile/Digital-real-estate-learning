b4fb6a721b3ecc556c10c34e608f2095aa08c000
= <niccolosottile@outlook.com>
2022-12-02 10:08:07 +0000

    running fetch queries on OpenSea Testnet

--- fetch-collection.js
+++ fetch-collection.js
@@ -1,43 +1,54 @@
 const fetchOwnCollection = async () => {
-    const WALLET_ADDRESS = '0x704CD00cbB8BF91038dFCF8bC008D065DDF1D8F8';
-  
-    const options = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        //'X-API-KEY': process.env.OPENSEA_API as string,
-      },
-    };
-  
-    const collectionResponse = await fetch(
-      `https://api.opensea.io/api/v1/collections?asset_owner=${WALLET_ADDRESS}`,
+  const WALLET_ADDRESS = '0x704CD00cbB8BF91038dFCF8bC008D065DDF1D8F8';
+
+  const options = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      //'X-API-KEY': process.env.OPENSEA_API as string,
+    },
+  };
+
+  const collectionResponse = await fetch(
+    `https://testnets-api.opensea.io/api/v1/collections?asset_owner=${WALLET_ADDRESS}`,
+    options,
+  ).then(response => response.json());
+
+  const collection = collectionResponse.map(item => ({
+    details: item.description,
+    slug: item.slug,
+    name: item.name,
+    contractAddress: item.primary_asset_contracts[0].address,
+    owned: [],
+  }));
+
+  for (const iterator of collection) {
+    // Timeout between each API GET because they are rate limited
+    await new Promise(resolve => setTimeout(resolve, 10000)); 
+
+    const assetsResponse = await fetch(
+      `https://testnets-api.opensea.io/api/v1/assets?owner=${WALLET_ADDRESS}&asset_contract_addresses=${iterator.contractAddress}&include_orders=false`,
       options,
     ).then(response => response.json());
-  
-    const collection = collectionResponse.map(item => ({
-      details: item.description,
-      slug: item.slug,
-      name: item.name,
-      contractAddress: item.primary_asset_contracts[0].address,
-      owned: [],
-    }));
-  
-    for (const iterator of collection) {
-      const assetsResponse = await fetch(
-        `https://api.opensea.io/api/v1/assets?owner=${WALLET_ADDRESS}&asset_contract_address=${iterator.contractAddress}&include_orders=false`,
-        options,
-      ).then(response => response.json());
-  
-      iterator.owned = assetsResponse.assets
-        .map(item => ({
-          name: item.name,
-          img: item.image_url,
-          id: item.token_id,
-        }))
-        .filter(item => item.name && item.img);
-    }
-  
-    return collection;
+
+    iterator.owned = assetsResponse.assets
+      .map(item => ({
+        name: item.name,
+        img: item.image_url,
+        id: item.token_id,
+      }))
+      .filter(item => item.name && item.img);
+  }
+
+  return collection;
 };
 
-console.log("ns1");
+async function processOwnCollection () {
+  const collection = await fetchOwnCollection();
+
+  for (const iterator of collection) {
+    console.log(iterator.owned);
+  }
+}
+
+processOwnCollection();
