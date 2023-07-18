4d4668a2f326658dbf74c3451b916251721c76e8
= <niccolosottile@outlook.com>
2023-07-18 18:45:41 +0100

    Configured new project structure and developed method to save data into dataset file

---
+++ data/collectData.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+
+function saveToLocal(newData) {
+  const filePath = path.join(__dirname, 'dataset.json');
+
+  // Check if the file exists
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    let data = [];
+
+    // If the file exists, read it and parse it to a JavaScript object
+    if (!err) {
+      const oldDataRaw = fs.readFileSync(filePath, 'utf8');
+      data = JSON.parse(oldDataRaw);
+    }
+    // Concatenate the old and new data
+    data = data.concat(newData);
+
+    // Write the full data back into the file
+    fs.writeFile(filePath, JSON.stringify(data), (err) => {
+      if (err) throw err;
+      console.log('Data saved to dataset.json');
+    });
+  });
+}
+
+fetchTransactions()
+  .then(newData => {
+    saveToLocal(newData);
+  })
+  .catch(error => console.error(error));
---
+++ model/train.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_json('data/dataset.json')
+
+# Continue with training
